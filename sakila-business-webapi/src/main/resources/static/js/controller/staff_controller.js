9091b7ca2cb6f0f8668d0105d2d7c1d88df92165
nrasolom <ptitbilly@MacBook-Pro-de-Billy.local>
2016-01-12 16:18:26 +0100

    Vérification authentification + Redirection
    retour d’un boolean pour vérifier l’authentification
    +
    Redirection

--- sakila-business-webapi/src/main/resources/static/js/controller/staff_controller.js
+++ sakila-business-webapi/src/main/resources/static/js/controller/staff_controller.js
@@ -2,24 +2,32 @@
 
 App.controller('StaffController', ['$scope', 'StaffService', function($scope, StaffService) {
 	var self = this;
+    self.auth = false;
 	self.staff={username:'', password:''};
 	
-	self.submit = function() {
+	self.submit = function(callBack) {
         if(self.staff.username==''){
             console.log('Error username', self.staff);    
         }else{
             console.log('Username is ', self.staff.username);
             console.log('Password: ', self.staff.password);
             StaffService.logStaff(self.staff).then(
-            		function(data){
-            			console.log(data);
-            		},
-            		function(err){
-            			// échec
-            		});
+	    		function(data){
+	    			var status = data.username == null ? false : true;
+	    			callBack(status);
+	    		});
+            
         }
-        self.reset();
     };
+    
+    self.redir = function(status){
+    	if (status){
+    		window.location.href="/ActorManagement.html";
+    		console.log('tu seras être redirigé')
+    	}else{
+    		console.log('tu ne seras pas redirigé')
+    	}
+    }
     
     self.reset = function(){
     	self.staff={username:'', password:''};
