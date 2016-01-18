d22f8d688dd2dc539f214f2d6ea72051cc62d3ee
Loïc <ldivad@isep.fr>
2016-01-18 15:58:21 +0100

    inventory By film

--- sakila-business-webapi/src/main/resources/static/js/service/inventory_service.js
+++ sakila-business-webapi/src/main/resources/static/js/service/inventory_service.js
@@ -3,9 +3,9 @@
  */
 App.factory('InventoryService', ['$http', '$q', function($http, $q){
 	return {
-		getInventories: function(){
+		getInventoryByFilm: function(filmId){
 			console.log('Using the Inventory Service: getInventory.');
-			return $http.get('http://localhost:8080/getInventories/').then(
+			return $http.get('http://localhost:8080/getInventoryByFilm/' + filmId ).then(
 					function(res){
 						console.log('Sucess du service, getInventory ');
 						console.log(res.data);
@@ -18,7 +18,22 @@ App.factory('InventoryService', ['$http', '$q', function($http, $q){
 					}
 			);
 		},
-		creatInventory: function(inventory){
+		getInventories: function(){
+			console.log('Using the Inventory Service: getInventories.');
+			return $http.get('http://localhost:8080/getInventories/').then(
+					function(res){
+						console.log('Sucess du service, getInventories ');
+						console.log(res.data);
+						return res.data;
+					},
+					function(err){
+						console.log('Erreur du service, getInventories ');
+						console.log(err);
+						return $q.reject(err);
+					}
+			);
+		},
+		createInventory: function(inventory){
 			console.log('Using the Inventory Service: creatInventory.');
 			return $http.get('http://localhost:8080/creatInventory/', inventory).then(
 					function(res){
