043d724d0db82b1128f6fdd40c0931cc49f771e8
nrasolom <ptitbilly@MacBook-Pro-de-Billy.local>
2016-01-19 14:30:52 +0100

    Résolution conflit

--- sakila-business-webapi/src/main/resources/static/js/controller/film_controller.js
+++ sakila-business-webapi/src/main/resources/static/js/controller/film_controller.js
@@ -1,83 +1,61 @@
-<<<<<<< Updated upstream
-/**
- * 
- */
-App.controller('FilmController', ['$scope', 'FilmService', 'InventoryService', function($scope, FilmService, InventoryService) {
-	var self = this;
-	var film = {filmId:0, title:'', length: 0, quantity : 0};
-	var films = [];
-	
-	self.fetchAllFilms = function(){
-		FilmService.getFilms().then(
-			function(res){
-				self.films = res;
-				//self.setQuantity();
-			},
-			function(err){
-				console.log("Error: controller failed to get the films");
-			}
-		);
-	}
-	
-	self.createFilm = function(actor){
-		FilmService.createFilm(actor).then(
-				self.fetchAllFilms,
-				function(err){
-					console.log("Error: controlle failed to greate a film");
-=======
 'use strict';
 
-App.controller('FilmController', ['$scope', 'FilmService', function($scope, FilmService) {
+App.controller('FilmController', ['$scope', 'FilmService', 'InventoryService', function($scope, FilmService, InventoryService) {
 	var self = this;
 	self.film={filmId:null,title:'',description:'', releaseYear:'', rentalDuration:'', rentalRate:'', lenght:'', language:null, features:''};
 	self.films=[];
 	
 	self.fetchAllFilms = function(){
-		FilmService.fetchAllFilms()
-		.then(
-				function(d) {
-					self.films = d;
+		FilmService.fetchAllFilms().then(
+				function(res) {
+					self.films = res;
 				},
-				function(errResponse){
-					console.error('Error while fetching Currencies');
->>>>>>> Stashed changes
+				function(err){
+					console.log("Error: controller failed to get the films");
 				}
 		);
 	};
 
 	self.createFilm = function(film){
-		FilmService.createFilm(film)
-		.then(
+		FilmService.createFilm(film).then(
 				self.fetchAllFilms, 
-				function(errResponse){
-					console.error('Error while creating Film.');
+				function(err){
+					console.log("Error: controller failed to greate a film");
 				}	
 		);
 	};
 
 	self.updateFilm = function(film){
-		FilmService.updateFilm(film)
-		.then(
+		FilmService.updateFilm(film).then(
 				self.fetchAllFilms, 
-				function(errResponse){
-					console.error('Error while updating Film.');
+				function(err){
+					console.log("Error while updating Film.");
 				}	
 		);
 	};
 
 	self.deleteFilm = function(filmId){
-		FilmService.deleteFilm(filmId)
-		.then(
+		FilmService.deleteFilm(filmId).then(
 				self.fetchAllFilms, 
-				function(errResponse){
-					console.error('Error while deleting Film.');
+				function(err){
+					console.log("Error while deleting Film.");
 				}	
 		);
 	};
 
 	self.fetchAllFilms();
-<<<<<<< Updated upstream
 	
+	self.addAvaibility = function(allFilms, allInventories){
+		allFilms.forEach(function(film){
+			var filtered = allInventories.filter(function(allInventory){
+				allInventory['filmId'] == film['filmId']
+			});
+			film['quantity'] = filtered.length;
+		});
+		return allFilms;
+	};
+	
+
 	self.setQuantity = function(){
 		self.films.forEach(function(film){
 			InventoryService.getInventoriesByFilmId(flim['filmId']).then(
@@ -88,12 +66,8 @@ App.controller('FilmController', ['$scope', 'FilmService', function($scope, Film
 		})
 	};
 	
-	
-}]);
-=======
-
 	self.submit = function() {
-		if(self.film.filmId==null){
+		if(self.film.filmId == null){
 			console.log('Saving New Film', self.film);    
 			self.createFilm(self.film);
 		}else{
@@ -132,68 +106,3 @@ App.controller('FilmController', ['$scope', 'FilmService', function($scope, Film
 	};
 
 }]);
-
-
-
-
-///**
-// * 
-// */
-//App.controller('FilmController', ['$scope', 'FilmService', function($scope, FilmService) {
-//	var self = this;
-//	var film = {filmId:0, title:'', length: 0};
-//	var films = [];
-//	
-//	self.fetchAllFilms = function(){
-//		FilmService.getFilms().then(
-//			function(res){
-//				self.films = res;
-//			},
-//			function(err){
-//				console.log("Error: controller failed to get the films");
-//			}
-//		);
-//	}
-//	
-//	self.createFilm = function(film){
-//		FilmService.createFilm(film).then(
-//				self.fetchAllFilms,
-//				function(err){
-//					console.log("Error: controlle failed to greate a film");
-//				}
-//		)
-//		
-//	};
-//	
-//	self.updateFilm = function(actor){
-//		FilmService.updateFilm(actor).then(
-//				self.fetchAllFilms,
-//				function(err){
-//					console.log("Error: controller failed to update a film");
-//				}
-//		)
-//	};
-//	
-//	self.deleteFilm = function(actorId){
-//		FilmService.deteleFilm(actorId).then(
-//				self.fetchAllFilms,
-//				function(err){
-//					console.log("Error: controller failed to deleted a film");
-//				}
-//		)
-//	};
-//	
-//	self.fetchAllFilms();
-//	
-//	self.addAvaibility = function(allFilms, allInventories){
-//		allFilms.forEach(function(film){
-//			var filtered = allInventories.filter(function(allInventory){
-//				allInventory['filmId'] == film['filmId']
-//			});
-//			film['quantity'] = filtered.length;
-//		});
-//		return allFilms;
-//	};
-//	
-//}]);
->>>>>>> Stashed changes
