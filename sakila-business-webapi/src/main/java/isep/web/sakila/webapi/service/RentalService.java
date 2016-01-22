e624ad3f084ee4d1b94664a4fc0875f78d88e72a
PHILIPPE Romain <romain.philippe78@free.fr>
2016-01-22 12:19:52 +0100

    fonction pour loic

--- sakila-business-webapi/src/main/java/isep/web/sakila/webapi/service/RentalService.java
+++ sakila-business-webapi/src/main/java/isep/web/sakila/webapi/service/RentalService.java
@@ -3,6 +3,7 @@ package isep.web.sakila.webapi.service;
 import java.text.ParseException;
 import java.util.List;
 
+import isep.web.sakila.webapi.model.RentalAndFilmWO;
 import isep.web.sakila.webapi.model.RentalWO;
 
 public interface RentalService {
@@ -10,6 +11,8 @@ public interface RentalService {
 	RentalWO findById(int id);
 
 	List<RentalWO> findByIdInventory(List<Integer> id);
+
+	List<RentalAndFilmWO> findByIdCustomer(int id);
 
 	void saveRental(RentalWO rentalWO) throws ParseException;
 
